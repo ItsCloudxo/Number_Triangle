176babf0279df808edd63a9a93cc0d1be75e72a8
ItsCloudxo <sebastian@nnsoft.dk>
2022-03-29 11:35:45 +0200

    Add project files.

---
+++ Number_Triangle/Program.cs
@@ -0,0 +1,14 @@
+﻿int rows = 5;
+string[] nbrs = { "1", "2", "3" };
+
+int nbrsCnt = 0;
+for (int i = 1; i <= rows; i++)
+{
+    string strOut = "";
+    for (int i2 = 1; i2 <= i; i2++)
+    {
+        strOut += nbrs[nbrsCnt];
+        nbrsCnt = (nbrsCnt + 1) % nbrs.Length;
+    }
+    Console.WriteLine(strOut);
+}
